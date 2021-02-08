fa5b180f7bcffc041e6c5268c39eecf9d3e7641c
Tristan816la <tristanq816@gmail.com>
2021-02-08 11:42:25 -0800

    fix login action creator

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -15,7 +15,6 @@ import {
   FormWrapper,
 } from "../../shared/Form";
 
-
 const SignUpTitle = styled.div`
   font-weight: bold;
   font-size: 24px;
@@ -30,23 +29,29 @@ const AccountBtn = styled(Button)`
   margin-top: 15px;
 `;
 const LoginSchema = Yup.object().shape({
-    email: Yup.string().email("Invalid email").required("Required"),
-    password: Yup.string()
-      .required("No password provided.")
-      .min(8, "Password is too short - should be 8 chars minimum.")
-      .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
-  });
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .required("No password provided.")
+    .min(8, "Password is too short - should be 8 chars minimum.")
+    .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+});
 
-const Login = () =>{
-    const dispatch = useDispatch();
-    let history = useHistory();
-    return(
-        <FormContainer>
-        <LeftFormContainer />
-        <RightFormContainer>
-        <button onClick = {()=>{history.push('/')}}>Signup</button>
-          <SignUpTitle>Log in to Embark</SignUpTitle>
-          <div style={{ marginBottom: "40px" }}>
+const Login = () => {
+  const dispatch = useDispatch();
+  let history = useHistory();
+  return (
+    <FormContainer>
+      <LeftFormContainer />
+      <RightFormContainer>
+        <button
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          Signup
+        </button>
+        <SignUpTitle>Log in to Embark</SignUpTitle>
+        <div style={{ marginBottom: "40px" }}>
           <GoogleButton
             onClick={() => {
               console.log("Google button clicked");
@@ -54,50 +59,49 @@ const Login = () =>{
           />
         </div>
         <OrSeperator />
-          <Formik
-            initialValues={{
-              email: "",
-              password: "",
-            }}
-            validationSchema={LoginSchema}
-            onSubmit={(values) => {
-              const { email, password } = values;
-              const oldUser = {
-                email,
-                password,
-              };
-              dispatch(loginUser(oldUser, history));
-              history.push('/landing')
-            }}
-            validateOnBlur={false}
-            validateOnChange={false}
-          >
-            {({ errors }) => (
-              <FormWrapper>
-                <Field
-                  name="email"
-                  placeholder="Email"
-                  as={TextField}
-                  margin="normal"
-                  helperText={errors.email}
-                  error={!!errors.email}
-                ></Field>
-                <Field
-                  name="password"
-                  placeholder="Password"
-                  as={TextField}
-                  margin="normal"
-                  helperText={errors.password}
-                  error={!!errors.password}
-                  type="password"
-                ></Field>
-                <AccountBtn type="submit">Log in</AccountBtn>
-              </FormWrapper>
-            )}
-          </Formik>
-        </RightFormContainer>
-      </FormContainer>
-    )
-}
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          validationSchema={LoginSchema}
+          onSubmit={(values) => {
+            const { email, password } = values;
+            const oldUser = {
+              email,
+              password,
+            };
+            dispatch(loginUser(oldUser, history));
+          }}
+          validateOnBlur={false}
+          validateOnChange={false}
+        >
+          {({ errors }) => (
+            <FormWrapper>
+              <Field
+                name="email"
+                placeholder="Email"
+                as={TextField}
+                margin="normal"
+                helperText={errors.email}
+                error={!!errors.email}
+              ></Field>
+              <Field
+                name="password"
+                placeholder="Password"
+                as={TextField}
+                margin="normal"
+                helperText={errors.password}
+                error={!!errors.password}
+                type="password"
+              ></Field>
+              <AccountBtn type="submit">Log in</AccountBtn>
+            </FormWrapper>
+          )}
+        </Formik>
+      </RightFormContainer>
+    </FormContainer>
+  );
+};
 
 export default Login;
