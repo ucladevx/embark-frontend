a460c76855d6ec184bc89930d724c680141b5ba1
Jiahe Yan <yjh020711@ucla.edu>
2021-02-24 17:40:46 +0800

    updated the navbar based on the design on figma

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,16 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { colors } from '../shared/config';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { colors } from "../shared/config";
+
+// icons for navbar icons
+import { ReactComponent as EmbarkIcon } from '../images/navbar_embark_logo.svg';
+import { ReactComponent as UserIcon } from '../images/navbar_user_logo.svg';
+import { ReactComponent as DevXIcon } from '../images/navbar_club_logo.svg';
+import { ReactComponent as CollapseIcon } from '../images/navbar_collapse_icon.svg';
+
+// imports for search bar
+import { Autocomplete } from '@material-ui/lab';
+import { TextField } from '@material-ui/core';
 
 const NavBarWrapper = styled.div`
   display: flex;
@@ -15,21 +25,20 @@ const NavBarWrapper = styled.div`
 `;
 const NavBarLogo = styled.div`
   border-radius: 50%;
-  background-color: ${colors.gray};
   height: 38px;
   width: 38px;
   display: grid;
   place-items: center;
-  margin: auto 20px;
+  margin: auto 0px auto 85px;
 `;
 const SearchBar = styled.input`
   background-color: ${colors.gray};
-  width: 530px;
+  width: 610px;
   height: 30px;
-  border-radius: 15px;
+  border-radius: 5px;
   display: flex;
   justify-content: flex-start;
-
+  margin: auto 0px auto 0px;
   align-items: center;
   padding-left: 20px;
   &::placeholder {
@@ -42,25 +51,79 @@ const UserLogo = styled.div`
   border-radius: 50%;
   width: 38px;
   height: 38px;
-  margin: auto 10px;
-  background-color: ${colors.gray};
+  margin: auto 0px auto 0px;
+`;
+const CollapseLogo = styled.div`
+  border-radius: 50%;
+  height: 38px;
+  width: 38px;
+  display: grid;
+  place-items: center;
+  margin: auto 80px auto 0px;
 `;
 
-const NavBar = () => {
-  const [search, setSearch] = useState('');
+const sampleSuggestions = [
+  {
+    title: 'DevX',
+    value: 'DevX'
+  },
+  {
+    title: 'Computer Science',
+    value: 'Computer Science'
+  },
+  {
+    title: 'Embark',
+    value: 'Embark'
+  }
+];
 
-  const handleChange = (e) => {
+const NavBar = () => {
+  const [search, setSearch] = useState("");
+  const [showList, setShowList] = useState(false);
+
+  const handleSearchChange = (e) => {
+    // just show suggestions 
+    // and console log for now
     setSearch(e.target.value);
+    console.log(search);
+    setShowList(true);
   };
+
+  const handleEmbarkIconClick = (e) => {
+    // just console log for now
+    console.log('Embark Icon Clicked')
+  }
+
+  const handleUserIconClick = (e) => {
+    // just console log for now
+    console.log('User Icon Clicked')
+  }
+
+  const handleCollapseIconClick = (e) => {
+    // just console log for now
+    console.log('Collapse Icon Clicked')
+  }
 
   return (
     <>
       <NavBarWrapper>
-        <NavBarLogo></NavBarLogo>
-        <div style={{ display: 'flex', flexGrow: 2, justifyContent: 'center' }}>
-          <SearchBar placeholder="Search Embark" onChange={handleChange} />
-        </div>
-        <UserLogo></UserLogo>
+        <NavBarLogo onClick={handleEmbarkIconClick}><EmbarkIcon /></NavBarLogo>
+        <section style={{ display: "flex", flexGrow: 2, justifyContent: "left" }}>
+          <div>
+            <Autocomplete
+              id="Search"
+              options={showList ? sampleSuggestions : []}
+              placeholder='Search'
+              freeSolo
+              getOptionLabel={(option) => option.title}
+              style={{ width: 610, backgroundColor: '#EDEDED' }}
+              onInputChange={handleSearchChange}
+              renderInput={(params) => <TextField {...params} placeholder='Search' size='small' onBlur={()=>setShowList(false)} variant="outlined" />}
+            />
+          </div>
+        </section>
+        <UserLogo onClick={handleUserIconClick}><UserIcon /></UserLogo>
+        <CollapseLogo onClick={handleCollapseIconClick}><CollapseIcon /></CollapseLogo>
       </NavBarWrapper>
     </>
   );
