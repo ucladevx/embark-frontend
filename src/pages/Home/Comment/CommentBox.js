0d7f3ca9f6cc877deed9cd5411c0e29e6ed6ab20
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2021-11-13 16:03:33 -0800

    check for null comment

--- src/pages/Home/Comment/CommentBox.js
+++ src/pages/Home/Comment/CommentBox.js
@@ -37,13 +37,19 @@ const CommentBox = ({ comments }) => {
       )}
       {comments &&
         comments.map((c, i) => {
-          if (i >= start && i < start + 4)
+          if (i >= start && i < start + 4) {
+            console.log(c, i);
+            if (c === null) {
+              return <div></div>;
+            }
             return (
               <PreviousCommentItem key={c._id || i}>
                 <PreviousCommentAvatar></PreviousCommentAvatar>
                 <div>
                   <PreviousCommentContent bgcolor={colors.gray1}>
-                    <PreviousCommentTitle>{c.authorEmail}</PreviousCommentTitle>
+                    <PreviousCommentTitle>
+                      {c.authorEmail ? c.authorEmail : c.author}
+                    </PreviousCommentTitle>
                     <Linkify>
                       <PreviousCommentText>{c.body}</PreviousCommentText>
                     </Linkify>
@@ -67,7 +73,7 @@ const CommentBox = ({ comments }) => {
                 </div>
               </PreviousCommentItem>
             );
-          else return <React.Fragment key={i}></React.Fragment>;
+          } else return <React.Fragment key={i}></React.Fragment>;
         })}
       {comments && comments.length > start + 4 && (
         <ViewCommentLink onClick={() => setStart(start + 4)}>
